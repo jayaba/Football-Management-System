24ed23ae4491618626c6261f3379a532d0fc2d10
Eranga S Jayaba <66355675+jayaba@users.noreply.github.com>
2021-06-07 00:19:51 +0530

    Add files via upload

---
+++ MyGymManagerTest.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyGymManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void addMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sort() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void save() {
+    }
+}
